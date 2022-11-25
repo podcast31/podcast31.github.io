7a6f9a18349decd1b3c5c7dfe1425e7cbddc3aa8
podcast31 <118902656+podcast31@users.noreply.github.com>
2022-11-26 00:33:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function msg2() {
+  if (confirm("Email: podcast31ilo@gmail.com\nKliknij OK, aby utworzyć kopię roboczą")) {
+    window.open('mailto:podcast31ilo@gmail.com', '_blank').focus();
+  } else {
+  }
+}
